6803bad5221d4499da06439852d7734290f8deef
phee <p037056n@student.staffs.ac.uk>
2025-09-16 14:59:46 +0100

    logout button

--- src/frontend/app/components/navbar.tsx
+++ src/frontend/app/components/navbar.tsx
@@ -23,62 +23,19 @@ export  const NavBar = () => {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
+          <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">
-                  Action
-                </a>
-                <a className="dropdown-item" href="#">
-                  Another action
-                </a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </div>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#">
-                Disabled
+              <a className="nav-link" href="http://localhost:5173/auth/logout">
+                <img
+                  src="/logout.png"
+                  alt="logout"
+                  className="d-inline-block align-top"
+                  style={{ paddingLeft: "10px", width: "1.5vw"}}
+                />
+                Logout
               </a>
             </li>
           </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button
-              className="btn btn-outline-success my-2 my-sm-0"
-              type="submit"
-            >
-              Search
-            </button>
-          </form>
         </div>
       </nav>
     </>
--- src/frontend/app/components/sidebar.tsx
+++ src/frontend/app/components/sidebar.tsx
@@ -3,7 +3,7 @@ export const SideBar = () => {
     <div className="sidebar border-end">
       <ul className="sidebar-nav">
         <li className="nav-item">
-          <a className="nav-link left-align" href="/home">
+          <a className="nav-link left-align" href="/">
             <img
               src="/dashboard.png"
               height="30"
