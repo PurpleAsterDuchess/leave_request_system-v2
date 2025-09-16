ef4d517e429920560ebaf52fec7127f100565b0d
phee <p037056n@student.staffs.ac.uk>
2025-09-16 14:35:23 +0100

    fix logout

--- src/frontend/app/routes/auth/logout.tsx
+++ src/frontend/app/routes/auth/logout.tsx
@@ -1,19 +1,18 @@
-import { type MetaFunction } from "react-router";
-import { redirect } from "react-router";
+import { type MetaFunction, redirect } from "react-router";
 import { logout } from "../../services/session.server";
 import type { Route } from "./+types/logout";
 
- const meta: MetaFunction = () => {
+export const meta: MetaFunction = () => {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Logout" },
+    { name: "description", content: "Logging you out..." },
   ];
 };
 
-export async function action({ request }: Route.ActionArgs) {
+export async function loader({ request }: Route.LoaderArgs) {
   return logout(request);
 }
 
-export async function loader({ request }: Route.LoaderArgs) {
-  return redirect("/auth/login");
+export async function action({ request }: Route.ActionArgs) {
+  return logout(request);
 }
